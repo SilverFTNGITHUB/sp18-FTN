f167d9a1ad3d5c3df1e3ef43ffec1a49276f3968
SilverFTNGITHUB <silverftn163@163.com>
2022-02-28 22:47:16 +0800

    complete first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int cumulation = 0;
+		for(int i = 0; i < 10; i++) {
+			cumulation = cumulation + i;
+			System.out.println(cumulation);
+		}
+		String test = "5" + "test";
+		System.out.println(test);
+	}
+}
